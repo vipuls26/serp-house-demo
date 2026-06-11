d2d7ae51d0a3d98649c9a3c550e0024d8619faee
vipuls26 <vipuls@patoliyainfotech.com>
2026-06-11 19:22:56 +0530

    add header  & footer +  PrimeIcons,

---
+++ app/components/common/AppHeader.vue
@@ -0,0 +1,105 @@
+<template>
+    <nav class="relative bg-white border-b border-gray-100 shadow-sm">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="flex items-center justify-between h-16 pt-2">
+
+                <div class="flex shrink-0 items-center">
+                    <a href="/" class="flex items-center justify-center font-bold">
+                        <img src="/images/serphouse-logo.png" class="h-10 lg:h-14 w-auto" alt="logo" />
+                    </a>
+
+                </div>
+
+                <div class="hidden lg:flex items-center space-x-4 text-sm font-medium text-gray-600">
+
+                    <button class="flex items-center space-x-1 text-indigo-500 hover:text-indigo-600 transition">
+                        <span>Product</span>
+                        <i class="pi pi-angle-down text-xs ml-1"></i>
+                    </button>
+
+                    <button class="flex items-center space-x-1 hover:text-indigo-500 transition">
+                        <span>Features</span>
+                        <i class="pi pi-angle-down text-xs ml-1"></i>
+                    </button>
+
+                    <a href="#" class="hover:text-indigo-500 transition">Try Demo</a>
+                    <a href="#" class="hover:text-indigo-500 transition">Pricing</a>
+
+                    <button class="flex items-center space-x-1 hover:text-indigo-500 transition">
+                        <span>Use Cases</span>
+                        <i class="pi pi-angle-down text-xs ml-1"></i>
+                    </button>
+
+                    <a href="#" class="hover:text-indigo-500">Documentation</a>
+                    <span class="h-4 w-px bg-gray-200"></span>
+
+                    <a href="#" class="hover:text-indigo-500 transition">Contact Sales</a>
+                    <a href="#" class="flex items-center space-x-1 text-indigo-500 hover:text-indigo-600 transition">
+                        <span>Login</span>
+                        <i class="pi pi-angle-right text-xs"></i>
+                    </a>
+
+                    <a href="#"
+                        class="bg-indigo-500 hover:bg-indigo-600 text-white font-medium px-5 py-2.5 rounded-full transition shadow-sm">
+                        Free Sign Up
+                    </a>
+                </div>
+
+                <div class="lg:hidden flex items-center space-x-4">
+                    <button @click="toggleMenu" class="text-gray-500 hover:text-gray-600 focus:outline-none">
+
+                        <i :class="['pi', isOpen ? 'pi-times' : 'pi-bars', 'text-2xl']"></i>
+                    </button>
+                    <a href="#"
+                        class="bg-indigo-500 hover:bg-indigo-600 text-white font-semibold px-3 py-2 rounded-full transition shadow-sm">
+                        Free Sign Up
+                    </a>
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+        <div v-show="isOpen" class="lg:hidden absolute w-full bg-white border-b border-gray-200 shadow-lg left-3">
+
+            <button class="w-full text-left py-2 text-indigo-500 hover:text-indigo-600">
+                <span>Product</span>
+                <i class="pi pi-angle-down text-xs ml-1"></i>
+            </button>
+
+            <button class="w-full text-left py-2 hover:text-indigo-500">
+                <span>Features</span>
+                <i class="pi pi-angle-down text-xs ml-1"></i>
+            </button>
+
+
+            <a href="#" class="block py-2 hover:text-indigo-500">Try Demo</a>
+
+            <a href="#" class="block py-2 hover:text-indigo-500">Pricing</a>
+
+            <button class="w-full text-left py-2 hover:text-indigo-500">
+                <span>Use Cases</span>
+                <i class="pi pi-angle-down text-xs ml-1"></i>
+            </button>
+
+            <a href="#" class="block py-2 hover:text-indigo-500">Documentation</a>
+
+            <a href="#" class="block py-2 hover:text-indigo-500">Contact Sales</a>
+
+            <a href="#" class="block py-2 text-indigo-500">Login</a>
+
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const isOpen = ref(false);
+
+const toggleMenu = () => {
+    isOpen.value = !isOpen.value;
+};
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,22 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['./app/assets/css/main.css'],
+  css: [
+    './app/assets/css/main.css',
+    'primeicons/primeicons.css'
+  ],
+
   vite: {
     plugins: [
       tailwindcss()
-    ]
-  }
+    ],
+   optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit'
+      ]
+    }
+  },
+
+  modules: ['@nuxt/icon']
 })
